fba87b480ba728d9816b4087fc2cfab3cbf64302
rugazinedine <0520005677@cikal.co.id>
2025-04-10 08:48:01 +0700

    style: [RZ] Create login page

---
+++ lib/pages/Login/login_page.dart
@@ -0,0 +1,268 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+const mainBlue = Color.fromRGBO(8, 76, 172, 1);
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 60,
+              ),
+              const Text(
+                'Login here',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: mainBlue,
+                  fontFamily: 'Poppins',
+                ),
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Welcome back you\'ve\nbeen missed!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.black87,
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(height: 70),
+
+              // Username TextField
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 12,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontFamily: 'Poppins'),
+                  decoration: InputDecoration(
+                    hintText: 'Username',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.grey[400],
+                      fontSize: 15,
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.person_outline,
+                      color: mainBlue,
+                      size: 22,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: mainBlue,
+                        width: 1.5,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 16),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              // Password TextField
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.05),
+                      blurRadius: 12,
+                      offset: const Offset(0, 2),
+                    ),
+                  ],
+                ),
+                child: TextField(
+                  obscureText: true,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontFamily: 'Poppins'),
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.grey[400],
+                      fontSize: 15,
+                    ),
+                    prefixIcon: const Icon(
+                      Icons.lock_outline,
+                      color: mainBlue,
+                      size: 22,
+                    ),
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: BorderSide.none,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                      borderSide: const BorderSide(
+                        color: mainBlue,
+                        width: 1.5,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 16),
+                  ),
+                ),
+              ),
+              SizedBox(height: 14),
+
+              // Forgot Password
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    // Add forgot password logic here
+                  },
+                  child: const Text(
+                    'Forgot your password?',
+                    style: TextStyle(
+                        color: mainBlue,
+                        fontFamily: 'Poppins',
+                        fontSize: 11,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              // Sign in Button
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add login logic here
+                    // After successful login:
+                    // Get.offAllNamed(AppRoutes
+                    //     .bottomnav); // Navigate to bottom navigation after successful login
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: mainBlue,
+                    padding: const EdgeInsets.symmetric(vertical: 15),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 40),
+
+              // Create new account
+              Center(
+                child: GestureDetector(
+                  onTap: () {
+                    // Get.toNamed(AppRoutes.register);
+                  },
+                  child: const Text(
+                    'Create new account',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: 'Poppins',
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 90),
+
+              // Or continue with
+              const Center(
+                child: Text(
+                  'Or continue with',
+                  style: TextStyle(
+                    color: mainBlue,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w600,
+                    fontFamily: 'Poppins',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+
+              // Social Login Buttons
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _socialLoginButton('assets/images/google.png'),
+                  const SizedBox(width: 20),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _socialLoginButton(String iconPath) {
+    return Container(
+      padding: const EdgeInsets.all(12),
+      decoration: BoxDecoration(
+        color: Colors.grey[100],
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 1,
+            blurRadius: 4,
+            offset: const Offset(0, 2),
+          ),
+        ],
+      ),
+      child: Image.asset(
+        iconPath,
+        height: 24,
+        width: 24,
+      ),
+    );
+  }
+}
