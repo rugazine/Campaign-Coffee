de23e50c3de368a69b709eff333309f0cb280825
rugazinedine <0520005677@cikal.co.id>
2025-04-19 07:44:24 +0700

    feat: [RZ] adding binding in all pages

---
+++ lib/binding/binding.dart
@@ -0,0 +1,21 @@
+
+
+
+import 'package:campaign_coffee/pages/auth/Login/login_controller.dart';
+import 'package:campaign_coffee/pages/cart/controllers/cart_controller.dart';
+import 'package:campaign_coffee/pages/home/controllers/detail_controller.dart';
+import 'package:campaign_coffee/pages/home/controllers/home_controller.dart';
+import 'package:campaign_coffee/pages/order/controllers/order_controller.dart';
+import 'package:get/get.dart';
+
+class MyBindings extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => LoginController());
+    Get.lazyPut(() => HomeController());
+    Get.lazyPut(() => DetailController());
+    Get.lazyPut(() => CartController());
+    Get.lazyPut(() => OrderController());
+
+    }
+}
